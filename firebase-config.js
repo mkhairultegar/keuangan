430d420dbaaf2437896b0a9b22be71a44e1c9904
M. Khairul Tegar <167969654+mkhairultegar@users.noreply.github.com>
2025-09-16 06:14:28 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   authDomain: "money-tracker-mkhairultegar.firebaseapp.com",
   databaseURL: "https://money-tracker-mkhairultegar-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "money-tracker-mkhairultegar",
-  storageBucket: "money-tracker-mkhairultegar.firebasestorage.app",
+  storageBucket: "money-tracker-mkhairultegar.appspot.com",
   messagingSenderId: "41631315085",
   appId: "1:41631315085:web:fed6c1846b93ff3cac9ec4"
 };
