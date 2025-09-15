3e2c0f34b9c4f44fe6cb06eae25b0477a06826b9
M. Khairul Tegar <167969654+mkhairultegar@users.noreply.github.com>
2025-09-16 05:53:20 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // Firebase Configuration
 // Ganti dengan config dari Firebase Console kamu
 const firebaseConfig = {
-  apiKey: "your-api-key-here",
-  authDomain: "your-project-id.firebaseapp.com", 
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "your-sender-id",
-  appId: "your-app-id"
+  apiKey: "AIzaSyBlO7XTiLA16gqtXXEnYYS-q0ytLxDEDYk",
+  authDomain: "money-tracker-mkhairultegar.firebaseapp.com",
+  databaseURL: "https://money-tracker-mkhairultegar-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "money-tracker-mkhairultegar",
+  storageBucket: "money-tracker-mkhairultegar.firebasestorage.app",
+  messagingSenderId: "41631315085",
+  appId: "1:41631315085:web:fed6c1846b93ff3cac9ec4"
 };
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
