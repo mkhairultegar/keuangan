68556ac629ffd0d02eaad830fbc6cee91d67b2b1
M. Khairul Tegar <167969654+mkhairultegar@users.noreply.github.com>
2025-09-16 04:50:14 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBlO7XTiLA16gqtXXEnYYS-q0ytLxDEDYk",
+  authDomain: "money-tracker-mkhairultegar.firebaseapp.com",
+  databaseURL: "https://money-tracker-mkhairultegar-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "money-tracker-mkhairultegar",
+  storageBucket: "money-tracker-mkhairultegar.firebasestorage.app",
+  messagingSenderId: "41631315085",
+  appId: "1:41631315085:web:fed6c1846b93ff3cac9ec4"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
